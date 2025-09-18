7a283022aab5d51f1c5e37ca720378bf63faba7a
dimell94 <dim.ell.094@gmail.com>
2025-09-18 14:03:29 +0300

    initial commit spring boot skeletion

---
+++ src/main/java/com/agnostik/agnostik_app/AgnostikAppApplication.java
@@ -0,0 +1,13 @@
+package com.agnostik.agnostik_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgnostikAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgnostikAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agnostik/agnostik_app/AgnostikAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agnostik.agnostik_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgnostikAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
