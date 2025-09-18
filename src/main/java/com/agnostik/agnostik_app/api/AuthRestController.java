6212a35fce0729be388658792d0f9d77d3abd540
dimell94 <dim.ell.094@gmail.com>
2025-09-18 18:19:08 +0300

    feat: add authentication rest controller

---
+++ src/main/java/com/agnostik/agnostik_app/api/AuthRestController.java
@@ -0,0 +1,36 @@
+package com.agnostik.agnostik_app.api;
+
+
+import com.agnostik.agnostik_app.authentication.AuthenticationService;
+import com.agnostik.agnostik_app.dto.AuthenticationRequestDTO;
+import com.agnostik.agnostik_app.dto.AuthenticationResponseDTO;
+import com.agnostik.agnostik_app.dto.UserRegisterDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthRestController {
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponseDTO> register(
+            @Valid @RequestBody UserRegisterDTO dto) {
+        AuthenticationResponseDTO response = authService.register(dto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponseDTO> login(
+            @Valid @ RequestBody AuthenticationRequestDTO dto) {
+        AuthenticationResponseDTO response = authService.login(dto);
+        return ResponseEntity.ok(response);
+    }
+}
