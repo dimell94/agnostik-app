990e9adffb88b5483d8a9a2fd0ed4350335a013d
dimell94 <dim.ell.094@gmail.com>
2025-09-18 15:08:43 +0300

    add: repository package and UserRepository

---
+++ src/main/java/com/agnostik/agnostik_app/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.agnostik.agnostik_app.repository;
+
+import com.agnostik.agnostik_app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
