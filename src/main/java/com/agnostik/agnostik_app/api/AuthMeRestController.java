26da15611aeb7357073746760f012356183284a6
dimell94 <dim.ell.094@gmail.com>
2025-09-21 17:43:40 +0300

    feat: add user readonly dto and authme protected controller

---
+++ src/main/java/com/agnostik/agnostik_app/api/AuthMeRestController.java
@@ -0,0 +1,19 @@
+package com.agnostik.agnostik_app.api;
+
+import com.agnostik.agnostik_app.dto.UserReadOnlyDTO;
+import com.agnostik.agnostik_app.model.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthMeRestController {
+
+    public ResponseEntity<UserReadOnlyDTO> me(Authentication authentication){
+
+        User user = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(new UserReadOnlyDTO(user.getUsername()));
+    }
+}
---
+++ src/main/java/com/agnostik/agnostik_app/dto/UserReadOnlyDTO.java
@@ -0,0 +1,10 @@
+package com.agnostik.agnostik_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserReadOnlyDTO {
+    private String username;
+}
