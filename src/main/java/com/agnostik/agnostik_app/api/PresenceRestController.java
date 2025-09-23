195bebc4b1e6fcc45d758b9c146c683fddf7660c
dimell94 <dim.ell.094@gmail.com>
2025-09-23 21:10:46 +0300

    feat: add presence rest controller

---
+++ src/main/java/com/agnostik/agnostik_app/api/PresenceRestController.java
@@ -0,0 +1,67 @@
+package com.agnostik.agnostik_app.api;
+
+import com.agnostik.agnostik_app.dto.UserReadOnlyDTO;
+import com.agnostik.agnostik_app.model.User;
+import com.agnostik.agnostik_app.service.PresenceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/presence/")
+@RequiredArgsConstructor
+@Slf4j
+public class PresenceRestController {
+
+    private final PresenceService presenceService;
+
+    @PostMapping("/leave")
+    public ResponseEntity<?> leave(@AuthenticationPrincipal UserReadOnlyDTO me){
+        presenceService.leave(me.getId());
+        return ResponseEntity.ok().body("User with id: " + me.getId() + " left");
+
+    }
+
+    @PostMapping("/lock")
+    public ResponseEntity<?> lock(@AuthenticationPrincipal UserReadOnlyDTO me){
+        presenceService.lock(me.getId());
+        return ResponseEntity.ok().body("User with id: " + me.getId() + " locked position");
+    }
+
+    @PostMapping("/unlock")
+    public ResponseEntity<?> unlock(@AuthenticationPrincipal UserReadOnlyDTO me){
+        presenceService.unlock(me.getId());
+        return ResponseEntity.ok().body("User with id: " + me.getId() + " unlocked position");
+    }
+
+    @PostMapping("/moveLeft")
+    public ResponseEntity<?> moveLeft(@AuthenticationPrincipal UserReadOnlyDTO me){
+        boolean moved = presenceService.moveLeft(me.getId());
+        if (!moved){
+            return ResponseEntity.status(409).body("CANNOT_MOVE_LEFT");
+        }
+
+        return ResponseEntity.ok().body("User with id: " + me.getId() + " moved left");
+    }
+
+    @PostMapping("/moveRight")
+    public ResponseEntity<?> moveRight(@AuthenticationPrincipal UserReadOnlyDTO me){
+        boolean moved = presenceService.moveRight(me.getId());
+        if (!moved){
+            return ResponseEntity.status(409).body("CANNOT_MOVE_RIGHT");
+        }
+
+        return ResponseEntity.ok().body("User with id: " + me.getId() + " moved right");
+    }
+
+    @GetMapping("/neighbors")
+    public ResponseEntity<?> neighbors(@AuthenticationPrincipal UserReadOnlyDTO me){
+        var neighbors = presenceService.getNeighbors(me.getId());
+        return ResponseEntity.ok(neighbors);
+    }
+}
