fa0f2c54cde352f44d19b1a07dc42fb2d2edec7a
dimell94 <dim.ell.094@gmail.com>
2025-09-23 18:20:23 +0300

    fix: add missing log info in register

--- src/main/java/com/agnostik/agnostik_app/authentication/AuthenticationService.java
+++ src/main/java/com/agnostik/agnostik_app/authentication/AuthenticationService.java
@@ -5,6 +5,7 @@ import com.agnostik.agnostik_app.core.exception.AppInvalidCredentialsException;
 import com.agnostik.agnostik_app.core.exception.AppObjectAlreadyExistsException;
 import com.agnostik.agnostik_app.dto.AuthenticationRequestDTO;
 import com.agnostik.agnostik_app.dto.AuthenticationResponseDTO;
+import com.agnostik.agnostik_app.dto.UserReadOnlyDTO;
 import com.agnostik.agnostik_app.dto.UserRegisterDTO;
 import com.agnostik.agnostik_app.model.User;
 import com.agnostik.agnostik_app.repository.UserRepository;
@@ -38,6 +39,10 @@ public class AuthenticationService {
         userRepository.save(user);
 
         String token = jwtService.generateToken(user.getId());
+
+        UserReadOnlyDTO newUser = UserReadOnlyDTO.from(user);
+
+        log.info("Register success: username='{}' (id={})",newUser.getUsername(), newUser.getId());
 
         return AuthenticationResponseDTO.builder()
                 .token(token)
