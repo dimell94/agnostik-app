7e6057f753a769d53cbee807145ee6007047bda3
dimell94 <dim.ell.094@gmail.com>
2025-11-28 15:50:26 +0200

    fix: add text max length logic

--- src/main/java/com/agnostik/agnostik_app/service/TextService.java
+++ src/main/java/com/agnostik/agnostik_app/service/TextService.java
@@ -13,14 +13,22 @@ public class TextService {
     private final Map<Long, String> texts = new ConcurrentHashMap<>();
 
     public void updateText(Long userId, String text) {
+        if (text == null) {
+            text = "";
+        }
+
+        if (text.length() > 6000) {
+            text = text.substring(0, 6000);
+        }
+
         texts.put(userId, text);
     }
 
-    public String getText(Long userId){
+    public String getText(Long userId) {
         return texts.getOrDefault(userId, "");
     }
 
-    public void clearText(Long userId){
+    public void clearText(Long userId) {
         texts.remove(userId);
     }
 }
